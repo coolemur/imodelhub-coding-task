1894ccdd4a185408e3930635e4202bf3d93b15f3
coolemur <coolemur@gmail.com>
2021-10-29 10:18:08 +0300

    Add UPDATE, DELETE methods #3

--- src/routes/clients.ts
+++ src/routes/clients.ts
@@ -79,4 +79,81 @@ router.post("/:path", verifyClient, async (req: Request, res: Response) => {
   }
 });
 
+// @route PUT /:path
+// @desc  Route PUT path
+// @access By client IDs
+router.put("/:path", verifyClient, async (req: Request, res: Response) => {
+  const clientId = req.headers['client-id'] as string;
+  const configuration = db.getConfiguration();
+  const path = `/${req.params.path}`;
+  const route = configuration.routes.find((route: Route) => route.sourcePath === path);
+
+  if (!route) {
+    return res.status(404).json({
+      error: `Route ${path} not found`
+    });
+  }
+
+  try {
+    const response = await axios.put(route.destinationUrl, req.body, {
+      headers: {
+        'client-id': clientId
+      }
+    });
+
+    res.location(route.destinationUrl);
+
+    return res.status(302).json(response.data);
+  } catch (error: any) {
+    if (error.message.includes("404")) {
+      return res.status(404).json({
+        error: `Route ${path} destination not found`
+      });
+    }
+
+    return res.status(500).json({
+      error: error.message
+    });
+  }
+});
+
+// @route DELETE /:path
+// @desc  Route DELETE path
+// @access By client IDs
+router.delete("/:path", verifyClient, async (req: Request, res: Response) => {
+  const clientId = req.headers['client-id'] as string;
+  const configuration = db.getConfiguration();
+  const path = `/${req.params.path}`;
+  const route = configuration.routes.find((route: Route) => route.sourcePath === path);
+
+  if (!route) {
+    return res.status(404).json({
+      error: `Route ${path} not found`
+    });
+  }
+
+  try {
+    const response = await axios.delete(route.destinationUrl, {
+      headers: {
+        'client-id': clientId
+      }
+    });
+
+    res.location(route.destinationUrl);
+
+    return res.status(302).json(response.data);
+
+  } catch (error: any) {
+    if (error.message.includes("404")) {
+      return res.status(404).json({
+        error: `Route ${path} destination not found`
+      });
+    }
+
+    return res.status(500).json({
+      error: error.message
+    });
+  }
+});
+
 export default router;
